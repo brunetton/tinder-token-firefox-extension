e0273270a590ff886da9237a9211cb6a00d2364e
Bruno <brunetton@brunetton.org>
2019-09-17 19:16:46 +0200

    Working version: copy to clipboard

--- background.js
+++ background.js
@@ -1,7 +1,31 @@
-function logURL(requestDetails) {
-  console.log("Chargement : " + requestDetails.url);
+function logURL(request) {
+  console.log("Chargement : " + request);
 }
 
-browser.webRequest.onBeforeRequest.addListener(logURL, {
-  urls: ["https://www.facebook.com/v2.10/dialog/oauth/confirm/"]
-});
+// browser.webRequest.onCompleted.addListener(logURL, {});
+
+function listener(details) {
+  let filter = browser.webRequest.filterResponseData(details.requestId);
+  let decoder = new TextDecoder("utf-8");
+  let encoder = new TextEncoder();
+
+  filter.ondata = async event => {
+    let str = decoder.decode(event.data, { stream: true });
+    const match = str.match(/token=(\w+)/);
+    if (match) {
+      token = match[1];
+      console.log("Found token: ", token);
+      await navigator.clipboard.writeText(token);
+    }
+    filter.disconnect();
+  };
+
+  return {};
+}
+browser.webRequest.onBeforeRequest.addListener(
+  listener,
+  {
+    urls: ["https://www.facebook.com/v2.10/dialog/oauth/confirm/"]
+  },
+  ["blocking"]
+);
